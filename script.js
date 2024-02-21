b79ba1fdda102bf37bc2da43ca8d87b460544c5d
Travis Anderson <travis.anderson30@gmail.com>
2024-02-21 08:14:46 -0700

    Set up hover even properly and console log a message. Need to figure out how to set a CSS property with JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const grid = document.querySelector(".grid");
+generateGrid()
 
-for (i = 0; i < 16; i++) {
-    let gridSquare = document.createElement("div");
-    gridSquare.className = "gridSquare";
-    grid.appendChild(gridSquare);
+const gridSquares = document.querySelectorAll(".gridSquare");
+gridSquares.forEach(square => {
+    square.addEventListener('mouseover', () => {
+        const squareStyle = getComputedStyle(square);
+        const backgroundColor = squareStyle.backgroundColor;
+        console.log(backgroundColor);
+        if (backgroundColor === "rgb(100, 149, 237)") {
+            console.log("It's totally cornflower blue");
+        }
+    })
+})
+
+function generateGrid() {
+    const grid = document.querySelector(".grid");
+
+    for (i = 0; i < 16; i++) {
+        let gridSquare = document.createElement("div");
+        gridSquare.className = "gridSquare";
+        grid.appendChild(gridSquare);
+    }
 }
