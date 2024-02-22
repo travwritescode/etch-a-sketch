df8b4eade3e7efaa493651084d1e40db4151e8e8
Travis Anderson <travis.anderson30@gmail.com>
2024-02-22 07:33:39 -0700

    Rework grid to match specs of 16x16

--- script.js
+++ script.js
@@ -8,16 +8,13 @@ gridSquares.forEach(square => {
         if (backgroundColor === "rgb(100, 149, 237)") {
             square.style.backgroundColor = "rgb(0, 0, 139)";
         }
-        else {
-            square.style.backgroundColor = "rgb(100, 149, 237)";
-        }
     })
 })
 
 function generateGrid() {
     const grid = document.querySelector(".grid");
 
-    for (i = 0; i < 16; i++) {
+    for (i = 0; i < 256; i++) {
         let gridSquare = document.createElement("div");
         gridSquare.className = "gridSquare";
         grid.appendChild(gridSquare);
