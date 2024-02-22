d0b690289432bf8b7ec66bea6f8986f462d26b4d
Travis Anderson <travis.anderson30@gmail.com>
2024-02-22 08:14:10 -0700

    Custom grid size

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-generateGrid()
-
-const gridSquares = document.querySelectorAll(".gridSquare");
-gridSquares.forEach(square => {
-    square.addEventListener('mouseover', () => {
-        const squareStyle = getComputedStyle(square);
-        const backgroundColor = squareStyle.backgroundColor;
-        if (backgroundColor === "rgb(100, 149, 237)") {
-            square.style.backgroundColor = "rgb(0, 0, 139)";
-        }
-    })
+const btnNewGrid = document.querySelector(".btnNewGrid");
+btnNewGrid.addEventListener('click', () => {
+    let gridSize = prompt("Please enter new grid size, no larger than 100.")
+    if (gridSize <= 100) {
+        generateGrid(gridSize);
+        createEtchASketch();
+    }
 })
 
-function generateGrid() {
+function createEtchASketch() {
+    const gridSquares = document.querySelectorAll(".gridSquare");
+    gridSquares.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            const squareStyle = getComputedStyle(square);
+            const backgroundColor = squareStyle.backgroundColor;
+            if (backgroundColor === "rgb(100, 149, 237)") {
+                square.style.backgroundColor = "rgb(0, 0, 139)";
+            }
+        })
+    })
+}
+
+function generateGrid(gridSize) {
     const grid = document.querySelector(".grid");
 
-    for (i = 0; i < 256; i++) {
+    for (i = 0; i < gridSize ** 2; i++) {
         let gridSquare = document.createElement("div");
         gridSquare.className = "gridSquare";
+        gridSquare.style.width = 960 / gridSize + "px";
+        gridSquare.style.height = 960 / gridSize + "px";
         grid.appendChild(gridSquare);
     }
 }
